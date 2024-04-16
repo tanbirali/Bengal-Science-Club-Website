5617b4746ed48f341bc51ae5e06b7ff5b884048f
tanbirali <tanbirali2004@gmail.com>
2024-04-16 11:41:32 +0530

    Courses page rendered

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,7 +1,72 @@
 import React from "react";
 
 const Courses = () => {
-  return <div>Courses</div>;
+  return (
+    <div>
+      <div className="p-6">
+        <h1 className="text-center font-bold text-2xl">Our upcoming courses</h1>
+        <div className="flex flex-col p-8 gap-5 lg:flex-row justify-center">
+          <div className="border p-4 rounded-md bg-gradient-to-b from-cyan-400 to-cyan-100">
+            <h1 className="text-white text-center">Class 9</h1>
+          </div>
+          <div className="border p-4 rounded-md bg-gradient-to-b from-cyan-400 to-cyan-100">
+            <h1 className="text-white text-center">Class 10</h1>
+          </div>
+          <div className="border p-4 rounded-md bg-gradient-to-b from-cyan-400 to-cyan-100">
+            <h1 className="text-white text-center">Class 11</h1>
+          </div>
+          <div className="border p-4 rounded-md bg-gradient-to-b from-cyan-400 to-cyan-100">
+            <h1 className="text-white text-center">Class 12</h1>
+          </div>
+          <div className="border p-4 rounded-md bg-gradient-to-b from-cyan-400 to-cyan-100">
+            <h1 className="text-white text-center">ANM & GNM</h1>
+          </div>
+        </div>
+      </div>
+      <div className="p-6">
+        <h2 className="text-center font-bold text-2xl">Our previous Courses</h2>
+        <div className="flex flex-col p-8 gap-5 lg:flex-row justify-center">
+          <div className="border p-4 rounded-md bg-gradient-to-b from-cyan-400 to-cyan-100">
+            <h1 className="text-white text-center">Class 9</h1>
+          </div>
+          <div className="border p-4 rounded-md bg-gradient-to-b from-cyan-400 to-cyan-100">
+            <h1 className="text-white text-center">Class 10</h1>
+          </div>
+          <div className="border p-4 rounded-md bg-gradient-to-b from-cyan-400 to-cyan-100">
+            <h1 className="text-white text-center">Class 11</h1>
+          </div>
+          <div className="border p-4 rounded-md bg-gradient-to-b from-cyan-400 to-cyan-100">
+            <h1 className="text-white text-center">Class 12</h1>
+          </div>
+          <div className="border p-4 rounded-md bg-gradient-to-b from-cyan-400 to-cyan-100">
+            <h1 className="text-white text-center">ANM & GNM</h1>
+          </div>
+        </div>
+      </div>
+      <div className="p-6">
+        <h2 className="text-center font-bold text-2xl">
+          Our most loved Courses
+        </h2>
+        <div className="flex flex-col p-8 gap-5 lg:flex-row justify-center">
+          <div className="border p-4 rounded-md bg-gradient-to-b from-cyan-400 to-cyan-100">
+            <h1 className="text-white text-center">Class 9</h1>
+          </div>
+          <div className="border p-4 rounded-md bg-gradient-to-b from-cyan-400 to-cyan-100">
+            <h1 className="text-white text-center">Class 10</h1>
+          </div>
+          <div className="border p-4 rounded-md bg-gradient-to-b from-cyan-400 to-cyan-100">
+            <h1 className="text-white text-center">Class 11</h1>
+          </div>
+          <div className="border p-4 rounded-md bg-gradient-to-b from-cyan-400 to-cyan-100">
+            <h1 className="text-white text-center">Class 12</h1>
+          </div>
+          <div className="border p-4 rounded-md bg-gradient-to-b from-cyan-400 to-cyan-100">
+            <h1 className="text-white text-center">ANM & GNM</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Courses;
